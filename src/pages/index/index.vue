745f150ca362d4d831f1a906a59ac7cf48d40e13
XIU2019 <1783833270@qq.com>
2020-05-24 18:05:42 +0800

    2020.5.24

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,248 +1,227 @@
 <template>
 
   <view>
-    <!--  <van-search-->
-    <!--    :value=" value "-->
-    <!--    shape="round"-->
-    <!--    background="#015478"-->
-    <!--    placeholder="请输入搜索关键词"-->
-    <!--  />-->
-    <!--  搜索框-->
-    <van-cell
-      size="large"
-      link-type="navigateTo"
-      url="/pages/search/main"
-    >
-      <van-search
-        :value="keyword"
-        disabled
-        input-align="center"
-        placeholder="请输入搜索关键词"
-      />
-    </van-cell>
-
-    <!--      v-bind:Click="onSearch($event)"-->
-
-    <!--    <van-search-->
-    <!--  :value="keyword"-->
-    <!--  placeholder="请输入搜索关键词"-->
-    <!--  use-action-slot-->
-    <!--&gt;-->
-    <!--  <view slot="action" @tap="onClick">搜索</view>-->
-    <!--</van-search>-->
-    <!--      <van-button type="default" @click="get">默认按钮</van-button>-->
-    <!--轮播图-->
-    <scroll-view scroll-y style="height: calc(35vh - 50px);" scroll-top="0">
-      <!--轮播图-->
-      <div class="ub-box ub-ver z-bg-color-fff">
-        <swiper class="swiper" indicator-dots="false" autoplay="false" interval="5000" duration="500">
-          <block v-for="(item, idx) in imgUrls" :key="idx">
-            <swiper-item>
-              <img :src="item" class="z-width-100-percent" mode="widthFix"/>
-            </swiper-item>
-          </block>
-        </swiper>
-      </div>
-    </scroll-view>
-    <!--    分类入口-->
-    <!--    <view>-->
-    <!--      <van-grid :column-num="3">-->
-    <!--        <van-grid-item v-for="value in 3" :key="value" icon="photo-o" text="文字"/>-->
-    <!--      </van-grid>-->
-    <!--    </view>-->
-    <!--    索引栏-->
-    <van-tabs v-bind:active="active" @change="onChange($event)">
-      <van-tab title="全部"/>
-      <van-tab title="早餐"/>
-      <van-tab title="午餐"/>
-      <van-tab title="晚餐"/>
-    </van-tabs>
-
-    <!--      使用宫格方式展示-->
-    <van-grid column-num="2">
-      <van-grid-item
-        use-slot
-        v-for="(item, idx) in goodList" :key="idx"
-        link-type="navigateTo"
-        url="/pages/products/main?index=${item.index}"
-      >
-        <img
-          style="width: 100%; height: 90px;"
-          v-bind:src="item.fileIds"
-        />
-        <p class="title">{{item.goodName}}
-          <van-tag plain color="#2bb0a6">{{item.category}}</van-tag>
-        </p>
-        <p class="text ">
-          <van-icon name="star" color="yellow"/>
-          {{item.score}}
-          月销售量：50
-        </p>
-        <p class='price'>￥{{item.price}}</p>
-      </van-grid-item>
-    </van-grid>
-
-    <!--猜你喜欢，商品列表-->
     <view>
-      <dl class="ub-box ub-col z-margin-top-6-px z-padding-all-8-px" style="background:#fff">
-        <p class="z-width-100-percent ub-box ub-ver" style="border-bottom:1px solid #eee">
-          <span class="z-font-size-12 z-color-888 z-lineHeight-36">—附近商家—</span>
-        </p>
-      </dl>
+      <van-tabs @change="onClickTitle">
+        <van-tab title="外卖">
+          <view v-for="(item, index) in orderList"
+                :key="index">
+            <van-cell>
+              <view slot="title">
+                {{index+1}}号
+              </view>
+              <view slot="right-icon" class="cell-value">
+                {{item.paymentStatus}}
+              </view>
+            </van-cell>
+            <van-cell>
+              <van-row v-for="(item, _id) in item.goodList"
+                       :key=" idx">
+                <van-col span="6" offset="1">{{item.goodName}}</van-col>
+                <van-col span="16">× {{item.num}}</van-col>
+              </van-row>
+              <van-row>
+                <van-col span="6" offset="1">备注</van-col>
+                <van-col span="16">{{item.orderInfo}}</van-col>
+              </van-row>
+              <van-row>
+                <van-col span="24"> 实付￥{{item.totalMoney}}</van-col>
+              </van-row>
+              <van-row>
+                <van-col span="24" class="cell-value" @click="oderDetail(item._id)">查看详情</van-col>
+              </van-row>
+            </van-cell>
+            <van-cell>
+              <van-row>
+                <van-button type="default" size="small" @click="acceptOrders(item._id)">接受订单</van-button>
+                <van-button type="default" size="small" @click="refuseOrders(item._id)">拒绝接单</van-button>
+              </van-row>
+            </van-cell>
+          </view>
+        </van-tab>
+        <van-tab title="食堂">
+           <view v-for="(item, index) in orderList"
+                :key="index">
+            <van-cell>
+              <view slot="title">
+                {{index+1}}号
+              </view>
+              <view slot="right-icon" class="cell-value">
+                {{item.paymentStatus}}
+              </view>
+            </van-cell>
+            <van-cell>
+              <van-row v-for="(item, _id) in item.goodList"
+                       :key=" idx">
+                <van-col span="6" offset="1">{{item.goodName}}</van-col>
+                <van-col span="16">× {{item.num}}</van-col>
+              </van-row>
+              <van-row>
+                <van-col span="6" offset="1">备注</van-col>
+                <van-col span="16">{{item.orderInfo}}</van-col>
+              </van-row>
+              <van-row>
+                <van-col span="24"> 实付￥{{item.totalMoney}}</van-col>
+              </van-row>
+              <van-row>
+                <van-col span="24" class="cell-value" @click="oderDetail(item._id)">查看详情</van-col>
+              </van-row>
+            </van-cell>
+            <van-cell>
+              <van-row>
+                <van-button type="default" size="small" @click="acceptOrders(item._id)">接受订单</van-button>
+                <van-button type="default" size="small" @click="refuseOrders(item._id)">拒绝接单</van-button>
+              </van-row>
+            </van-cell>
+          </view>
+        </van-tab>
+        <van-tab title="退款">
+          退款
+        </van-tab>
+      </van-tabs>
     </view>
-
-    <!--   常规的商品卡片  -->
-    <van-card
-      desc="桂林理工大学屏风校区"
-      title="桂工一楼食堂"
-      tag="美食"
-      v-bind:thumb="imageURL"
-      thumb-link="/pages/products/main"
-    >
-      <view slot="tags">
-        <!--        <van-button size="mini">按钮</van-button>-->
-        <!--        <van-button size="mini">按钮</van-button>-->
-        <view class="text "> 起送￥10 配送￥1 人均￥10</view>
-      </view>
-      <view slot="tags">
-        <van-tag plain type="danger">满20减2</van-tag>
-        <van-tag plain type="danger">满30减5</van-tag>
-      </view>
-    </van-card>
   </view>
 
 </template>
 <script>
 
   export default {
-    computed: {
-      onReady: function () {
-        this.getGoodList()
-      },
-      //测试
-      // getList () {
-      //   const that = this
-      //   const db = wx.cloud.database()
-      //   const banner = db.collection('banner')
-      //   banner.get().then(res => {
-      //     console.log(res.data[0].fileID)
-      //   })
-      //     .catch(err => {
-      //       console.log(err)
-      //     })
-      // },
-      //响应分类导航组件的分类点击事件
-
-      //获取菜品信息
-      // getList () {
-      //   const that = this
-      //   const db = wx.cloud.database()
-      //   const banner = db.collection('good')
-      //   banner.get().then(res => {
-      //     console.log(res)
-      //     that.goodList = that.goodList.concat(res.data)
-      //     that.category = that.goodList.item.category
-      //     console.log('goodList:', that.goodList)
-      //     console.log('category:', that.category)   //打印分类
-      //   })
-      //     .catch(err => {
-      //       console.log(err)
-      //     })
-      // },
+    computed: {},
+    onReady: function () {
 
     },
-    data () {
+    onLoad() {
+      this.getOrderList()
+    },
+    data() {
       return {
-        imgUrls: [
-          'http://p1.meituan.net/codeman/826a5ed09dab49af658c34624d75491861404.jpg',
-          'http://p0.meituan.net/codeman/a97baf515235f4c5a2b1323a741e577185048.jpg'
-        ],
-        imageURL: 'https://img.yzcdn.cn/vant/cat.jpeg',
-        images: [],
-        goodList: [],
-        active: 0,
-        category: [],//分类数组
-        selectedCategory: '全部', //当前选中的分类ID，页面加载时默认选中的全部分类
-        keyword: '',
+        orderList: [],
+        selectedTitle: '外卖配送'
+
       }
     },
     methods: {
-//       测试下载图片
-//       get () {
-//         wx.cloud.downloadFile({
-//           fileID: 'cloud://test-a1vlk.7465-test-a1vlk-1301795243/1586400400996.png', // 文件 ID
-//           success: res => {
-//             // 返回临时文件路径
-//             const that = this
-//             console.log(res.tempFilePath)
-//             that.images [0] = res.tempFilePath
-//             console.log(images[0])
-//           },
-//           fail: console.error
-//         })
-//
-//       },
-      //响应分类导航组件的分类点击事件
-      onChange (event) {
-        wx.showToast({
-          // title: `切换到标签 ${event.mp.detail.name}`,
-          title: `切换到分类${event.mp.detail.title}`,
-          icon: 'none'
-        })
-        //将用户点击的分类  放入selectedCategory中
-        this.selectedCategory = event.mp.detail.title,
-          console.log('selectedCategory:', this.selectedCategory)
+
+      onClickTitle(event) {
+        console.log(event.mp.detail)
+        let selectedTitle = event.mp.detail.title
+        if (selectedTitle === "外卖") {
+          this.selectedTitle = "外卖配送";
+          this.getOrderList()
+        } else if (selectedTitle === "食堂") {
+          this.selectedTitle = "食堂自取";
+          this.getOrderList()
+        } else {
+          this.selectedTitle = selectedTitle;
+          this.getOrderList();
+        }
 
       },
-
       //     获取列表
-      getGoodList () {
-        const that = this
-        const db = wx.cloud.database()
-        const good = db.collection('good')
-        if (that.selectedCategory === '全部') {
-          good.get().then(res => {
-            console.log(res)
-            that.goodList = [],
-              that.goodList = that.goodList.concat(res.data)
-            // that.category = that.goodList.item.category
-            console.log('goodList:', that.goodList)
-            // console.log('category:', that.category)   //打印分类
+      getOrderList() {
+        const that = this;
+        const db = wx.cloud.database();
+        const order = db.collection('order');
+        if (that.selectedTitle === '退款') {
+          order.where({
+            serviceStatus: that.selectedTitle,
+          }).where({
+              orderStatus: '等待接单',
+          }).get().then(res => {
+            console.log(res);
+            that.orderList = that.orderList.concat(res.data);
+          }).catch(err => {
+            console.log(err)
           })
-            .catch(err => {
-              console.log(err)
-            })
-        } else if (that.selectedCategory !== '全部' && that.selectedCategory !== '' && that.keyword !== '') {
-
-        } else if (that.selectedCategory !== '全部' && that.selectedCategory !== '') {
-          good.where({
-            category: that.selectedCategory,
+        } else if (that.selectedTitle === "外卖配送" || that.selectedTitle === "食堂自取") {
+          order.where({
+            orderTypes: that.selectedTitle,
+            orderStatus: '等待接单',
           })
             .get().then(res => {
-            console.log(res)
-            that.goodList = [],
-              that.goodList = that.goodList.concat(res.data)
-            console.log('goodList:', that.goodList)
+            console.log(res);
+            that.orderList = res.data;
           })
             .catch(err => {
               console.log(err)
             })
-        } else if (that.keyword !== '') {
-
         }
       },
-
+      //接受订单
+      acceptOrders(id) {
+        // console.log(id)
+        //跟新食堂管理员端的Order表
+        const db = wx.cloud.database();
+        db.collection('orderAdmit').where({orderId: id})
+          .update({
+              data: {
+                orderStatus: '已接单',
+              }
+            }
+          ).then(res => {
+          console.log(res)
+        }).catch(err => {
+            console.log(err)
+          })
+            db.collection('order').where({_id: id})
+          .update({
+              data: {
+                orderStatus: '商家接单',
+              }
+            }
+          ).then(res => {
+          console.log(res)
+        }).catch(err => {
+            console.log(err)
+          })
+      },
+      //  拒绝订单
+      refuseOrders(id) {
+        // console.log(id)
+           const db = wx.cloud.database();
+        db.collection('orderAdmit').where({orderId: id})
+          .update({
+              data: {
+                orderStatus: '拒绝接单',
+              }
+            }
+          ).then(res => {
+          console.log(res)
+        }).catch(err => {
+            console.log(err)
+          })
+            db.collection('order').where({_id: id})
+          .update({
+              data: {
+                orderStatus: '拒绝接单',
+              }
+            }
+          ).then(res => {
+          console.log(res)
+        }).catch(err => {
+            console.log(err)
+          })
+      }
+      ,
+      //  查看订单详情
+      oderDetail(id) {
+        console.log(id);
+        wx.redirectTo({
+          url: `/pages/orderDetail/main?order_id=${id}`,
+        })
+      }
+      ,
     },
   }
 </script>
 <style scoped>
-  .container {
+  .body {
     width: 100%;
-    height: 100vh;
-    background: #e8e8e8;
+    height: 100%;
+    background: #b2b2b2;
   }
 
-  .search {
+  .top {
     background: #f5f5f5;
     border-radius: 12px;
     padding: 5px 10px
@@ -303,8 +282,8 @@
     font-size: 18px;
   }
 
-  .text {
-    font-size: 14px;
+  .cell-value {
+    color: #ff2624 !important;
   }
 
 
